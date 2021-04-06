401e3d875fa80dedb53756c197df12dbd6b2f875
Louise Moncla <lhmoncla@gmail.com>
2021-04-06 16:21:17 -0700

    updating plotting scripts

--- scripts/Wisconsin-within-host-regression.R
+++ scripts/Wisconsin-within-host-regression.R
@@ -2,6 +2,8 @@
 library(rethinking)
 require(reshape2)
 require(ggplot2)
+library(ggridges)
+
 
 
 # read in dataframe
@@ -153,23 +155,19 @@ compare(m1,m3,m4,m5,m6,m7,m8)
 
 # ok, so from these comparisons, it looks like household pair is a better predictor than transmission pair, and that they contribute the same information in the model. So we should probably just use household pair and not transmission pair 
 
-# plot coefficients for model chosen
+# plot coefficients mean estimates with 95th percentiles
 precis(m8, digits=6, prob=0.95)
 plot(precis(m8, prob=0.95))
 
 # plot!
-# set output of precis to dataframe
+# set output of precis to dataframe; this will let us plot the mean coefficient estimates
+# with the 95% percentiles
 df1 = data.frame(precis(m8, digits=6, prob=0.95))
 
 # reindex dataframe
 df2 <- cbind(measurement = rownames(df1), df1)
 rownames(df2) <- 1:nrow(df2)
 colnames(df2) = c("measurement","mean","stdev","lower95","upper95")
-
-post_m8 <- extract.samples(m8)
-post_m8 <- subset (post_m8, select = -betaGeo)
-
-post_m8_melt = melt(post_m8)
 
 df2$measurement = gsub("betaHousehold","household",df2$measurement)
 df2$measurement = gsub("betaGeo","distance",df2$measurement)
@@ -214,37 +212,155 @@ p <- ggplot(data=df2, aes(y=measurement_f, x=mean, color=measurement_f)) +
   theme(panel.background=element_rect(fill=NA))+
   theme(legend.key=element_rect(fill=NA))
 
-ggsave("regression_coefficients-2020-02-24.pdf", width = 6, height = 4, device = "pdf", path = "../figures/individual-pdfs", dpi = 300)
+ggsave("regression_coefficients-2020-02-25.pdf", width = 6, height = 4, device = "pdf", path = "../figures/individual-pdfs", dpi = 300)
 p 
 
-# UP TO HERE I AM UP TO DATE 
-# extract samples from model m6, which has just intercept and Marshallese status
-# use these samples to calculate the difference in having descendants between Marshallese and non-Marshallese tips 
-# extract samples from the model fitting in m2 and m3; these samples form the posterior distributions for our variables we estimated
 
-plot(df$prop_snvs_shared, df$household_same)
+# now try plotting the same figure but plotting the posteriors instead
+post_m8 <- extract.samples(m8)
+colnames(post_m8) = c("intercept","divergence","clade","distance","household","variance")
+
+# gather the 95% HPD estimate for each variable
+hpdi_int = HPDI(post_m8$intercept, prob=0.95)
+hpdi_var = HPDI(post_m8$variance, prob=0.95)
+hpdi_div = HPDI(post_m8$divergence, prob=0.95)
+hpdi_clade = HPDI(post_m8$clade, prob=0.95)
+hpdi_dist = HPDI(post_m8$distance, prob=0.95)
+hpdi_hh = HPDI(post_m8$household, prob=0.95)
+
+# separate into high and low bounds of HPDI
+lower_hpdi_int = hpdi_int[[1]]
+upper_hpdi_int = hpdi_int[[2]]
+lower_hpdi_var = hpdi_var[[1]]
+upper_hpdi_var = hpdi_var[[2]]
+lower_hpdi_div = hpdi_div[[1]]
+upper_hpdi_div = hpdi_div[[2]]
+lower_hpdi_clade = hpdi_clade[[1]]
+upper_hpdi_clade = hpdi_clade[[2]]
+lower_hpdi_dist = hpdi_dist[[1]]
+upper_hpdi_dist = hpdi_dist[[2]]
+lower_hpdi_hh = hpdi_hh[[1]]
+upper_hpdi_hh = hpdi_hh[[2]]
 
 
-# to describe the distribution of predicted probability of having descendants for Marshallese and non-Marshallese tips, we need to apply the logistic function to the linear model that generated the data; Marshallese is coded as a 1, so it will be calculated using a sample from the posterior for the intercept + a sample from the posterior for the slope of Marshallese; for non-Marshallese, this is coded as a 0, so bm drops out
-prop_shared.household_same.pred <- post_m8$a + post_m8$betaHousehold +  post_m8$betaDiv+ post_m8$betaClade
-prop_shared.household_diff.pred <- post_m8$a + post_m8$betaDiv + post_m8$betaClade
+# now, melt it 
+post_m8_melt = melt(post_m8)
+post_m8_melt$variable_f = factor(post_m8_melt$variable, levels=c("divergence","clade","household","distance","variance","intercept"))
+
+# subest melted dataframe to include only vvalues in the HPDI
+post_m8_melt_hpdi <- post_m8_melt[!(post_m8_melt$variable=="intercept" & post_m8_melt$value > upper_hpdi_int),]
+post_m8_melt_hpdi <- post_m8_melt_hpdi[!(post_m8_melt_hpdi$variable=="intercept" & post_m8_melt_hpdi$value < lower_hpdi_int),]
+
+post_m8_melt_hpdi <- post_m8_melt_hpdi[!(post_m8_melt_hpdi$variable=="variance" & post_m8_melt_hpdi$value > upper_hpdi_var),]
+post_m8_melt_hpdi <- post_m8_melt_hpdi[!(post_m8_melt_hpdi$variable=="variance" & post_m8_melt_hpdi$value < lower_hpdi_var),]
+
+post_m8_melt_hpdi <- post_m8_melt_hpdi[!(post_m8_melt_hpdi$variable=="clade" & post_m8_melt_hpdi$value > upper_hpdi_clade),]
+post_m8_melt_hpdi <- post_m8_melt_hpdi[!(post_m8_melt_hpdi$variable=="clade" & post_m8_melt_hpdi$value < lower_hpdi_clade),]
+
+post_m8_melt_hpdi <- post_m8_melt_hpdi[!(post_m8_melt_hpdi$variable=="distance" & post_m8_melt_hpdi$value > upper_hpdi_dist),]
+post_m8_melt_hpdi <- post_m8_melt_hpdi[!(post_m8_melt_hpdi$variable=="distance" & post_m8_melt_hpdi$value < lower_hpdi_dist),]
+
+post_m8_melt_hpdi <- post_m8_melt_hpdi[!(post_m8_melt_hpdi$variable=="divergence" & post_m8_melt_hpdi$value > upper_hpdi_div),]
+post_m8_melt_hpdi <- post_m8_melt_hpdi[!(post_m8_melt_hpdi$variable=="divergence" & post_m8_melt_hpdi$value < lower_hpdi_div),]
+
+post_m8_melt_hpdi <- post_m8_melt_hpdi[!(post_m8_melt_hpdi$variable=="household" & post_m8_melt_hpdi$value > upper_hpdi_hh),]
+post_m8_melt_hpdi <- post_m8_melt_hpdi[!(post_m8_melt_hpdi$variable=="household" & post_m8_melt_hpdi$value < lower_hpdi_hh),]
 
 
-df_households = melt(data.frame(prop_shared.household_diff.pred, prop_shared.household_same.pred))
+p <- ggplot(data=post_m8_melt_hpdi, aes(x=value, y=variable_f, color=variable_f, fill=variable_f)) + 
+  geom_density_ridges(alpha=0.6, scale=1, rel_min_height=0.01, size=0.25, position=position_points_jitter(width=0.05,height=0), pointsize=0.5, point_alpha = 0.5) +
+  labs(x="\nposterior",y="coefficient\n")+
+  scale_color_manual(values=c(intercept=grey,variance=grey,distance=red,divergence=yellow,clade=blue,household=purple), guide=FALSE)+
+  scale_fill_manual(values=c(intercept=grey,variance=grey,distance=red,divergence=yellow,clade=blue,household=purple), guide=FALSE)+
+  #scale_y_continuous(breaks=seq(0,10000,1000), limits=c(0,10000))+
+  #scale_x_continuous(breaks=seq(0,0.3,0.1), limits=c(-0.05,0.3))+
+  theme(panel.grid.major.y=element_line(colour="black",size=0.1))+    
+  theme(panel.grid.major.x=element_line(colour=NA,size=NA))+
+  #theme(panel.grid.minor=element_line(colour=NA,size=NA))+    
+  #theme(strip.background = element_rect(colour=NA, fill=NA))+
+  theme(axis.line.x=element_line(colour="black"))+
+  theme(axis.line.y=element_line(colour="black"))+
+  theme(axis.title=element_text(size=12, vjust=5))+
+  theme(axis.text.y=element_text(size=10, colour="black"))+
+  theme(axis.text.x=element_text(size=10, colour="black", hjust=0.5))+
+  theme(legend.title=element_blank())+
+  #theme(panel.margin=unit(1, "lines"))+
+  #theme(plot.margin=unit(c(1,4,1,1),"cm"))+
+  theme(legend.key.size=unit(0.7, "cm"))+
+  theme(panel.background=element_rect(fill=NA))+
+  theme(legend.key=element_rect(fill=NA))
 
-ggplot(df_households)+ geom_density(aes(x=value, color=variable))+scale_color_manual(values=c("red","blue"))
+ggsave("regression_coefficients-posteriors-2020-03-25.png", width =2.75, height = 2, path = "src/ncov-WI-within-host/figures/individual-pdfs/", dpi = 300)
+p 
 
 
-# now do some implied predictions
-# dummy data 
-d.pred <- data.frame(
-  household_same = c(0,1)  # no, yes, yes, yes, yes, no
-)
 
-mu <- link(m6, data=data.frame(household_same=d.pred))
-mu.mean <- apply(mu,2,mean)
-mu.HPDI <- apply(mu,2,HPDI, prob=0.89)
+# Plot the full posterior distribution facetted, with alpha = HPDI
 
-# summarize
-pred.p <- apply(plants.ensemble$link, 2, mean)
-pred.p.PI <- apply(plants.ensemble$link, 2, PI)
+# add a column to store whether the value is within the HPDI; initialize with "yes"
+post_m8_melt$hpdi <- "yes"
+
+# add in "no"s for values outside HPDI
+post_m8_melt$hpdi[which(post_m8_melt$variable == "intercept" & post_m8_melt$value < lower_hpdi_int)] = "no"
+post_m8_melt$hpdi[which(post_m8_melt$variable == "intercept" & post_m8_melt$value > upper_hpdi_int)] = "no"
+
+post_m8_melt$hpdi[which(post_m8_melt$variable == "variance" & post_m8_melt$value < lower_hpdi_var)] = "no"
+post_m8_melt$hpdi[which(post_m8_melt$variable == "variance" & post_m8_melt$value > upper_hpdi_var)] = "no"
+
+post_m8_melt$hpdi[which(post_m8_melt$variable == "clade" & post_m8_melt$value < lower_hpdi_clade)] = "no"
+post_m8_melt$hpdi[which(post_m8_melt$variable == "clade" & post_m8_melt$value > upper_hpdi_clade)] = "no"
+
+post_m8_melt$hpdi[which(post_m8_melt$variable == "divergence" & post_m8_melt$value < lower_hpdi_div)] = "no"
+post_m8_melt$hpdi[which(post_m8_melt$variable == "divergence" & post_m8_melt$value > upper_hpdi_div)] = "no"
+
+post_m8_melt$hpdi[which(post_m8_melt$variable == "distance" & post_m8_melt$value < lower_hpdi_dist)] = "no"
+post_m8_melt$hpdi[which(post_m8_melt$variable == "distance" & post_m8_melt$value > upper_hpdi_dist)] = "no"
+
+post_m8_melt$hpdi[which(post_m8_melt$variable == "household" & post_m8_melt$value < lower_hpdi_hh)] = "no"
+post_m8_melt$hpdi[which(post_m8_melt$variable == "household" & post_m8_melt$value > upper_hpdi_hh)] = "no"
+
+
+# plot it. I'm going to have to do some sort of fancy things to give each posterior its own x-axis.
+
+# define the axis limits; this is calculated based on the min and max values in the posterior set, with a little bit of extra padding
+
+require(grid)
+library(grid)
+require(gridExtra)
+library(gridExtra)
+
+post_m8_melt$variable_f = factor(post_m8_melt$variable, levels=c("divergence","clade","household","distance","variance","intercept"))
+
+blank_data <- data.frame(variable_f = c("divergence","divergence","distance","distance","clade","clade","household","household","variance","variance","intercept","intercept"), x = c(-0.002,0.002,-0.0003,-0.0001,0.05,0.10,0.2,0.40,0.15,0.17,0.025,0.1), hpdi="yes")
+
+p_facet <- ggplot(data=post_m8_melt, aes(x=value, color=variable_f, fill=variable_f, group=variable_f)) + 
+  geom_density(alpha=0.8, size=0) +
+  facet_wrap(~variable_f, scales="free")+
+  geom_blank(data=blank_data, aes(x=x))+
+  labs(x="\nposterior estimate",y="density\n")+
+  scale_color_manual(values=c(intercept=grey,variance=grey,distance=red,divergence=yellow,clade=blue,household=purple), guide=FALSE)+
+  scale_fill_manual(values=c(intercept=grey,variance=grey,distance=red,divergence=yellow,clade=blue,household=purple), guide=FALSE)+
+  #scale_y_continuous(breaks=seq(0,10000,1000), limits=c(0,10000))+
+  #scale_x_continuous(breaks=seq(seqs_list[[variable_f]]))+
+  theme(panel.grid.major=element_line(colour=NA,size=NA))+    
+  theme(panel.grid.minor=element_line(colour=NA,size=NA))+
+  theme(panel.spacing.x=unit(4, "lines"))+
+  theme(panel.spacing.y=unit(3, "lines"))+
+  theme(strip.background = element_rect(colour=NA, fill=NA))+
+  theme(strip.text=element_text(size=20))+
+  theme(axis.line.x=element_line(colour="black"))+
+  theme(axis.line.y=element_line(colour="black"))+
+  theme(axis.title=element_text(size=20, vjust=5))+
+  theme(axis.text.y=element_blank())+
+  theme(axis.ticks.y=element_blank())+
+  theme(axis.text.x=element_text(size=16, colour="black", hjust=0.5))+
+  theme(legend.title=element_blank())+
+  #theme(panel.margin=unit(1, "lines"))+
+  #theme(plot.margin=unit(c(1,4,1,1),"cm"))+
+  theme(legend.key.size=unit(0.7, "cm"))+
+  theme(panel.background=element_rect(fill=NA))+
+  theme(legend.key=element_rect(fill=NA))
+
+ggsave("regression_coefficients-posteriors-facetted-2020-03-25.pdf", width = 12, height = 6, path = "src/ncov-WI-within-host/figures/individual-pdfs/", dpi = 300)
+p_facet 
+
